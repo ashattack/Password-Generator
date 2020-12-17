be5a92ce28c542af20e713a2b79e3e3f843a918f
Ashlyn Hicks <ash.giselle96@gmail.com>
2020-12-16 21:32:15 -0500

    added prompts and confirms

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+console.log(generateBtn)
+
+
 
 // Write password to the #password input
+function generatePassword(characterCount, numbers, specialCharacters, upperCase, lowerCase) {
+  return "password"
+}
+
 function writePassword() {
+  
+  var characterCount = prompt("How many characters do you want your password to be");
+  var numbers = confirm("Do you want numbers in your password?");
+  var specialCharacters = confirm("Do you want special characters in your password?");
+  var upperCase = confirm("Do you want upper case in your password?");
+  var lowerCase = confirm("Do you want lower case in your password?");
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
