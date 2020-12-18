88bbc1690f9f4c13b94f3ae452e72bdec22f25fa
Ashlyn Hicks <ash.giselle96@gmail.com>
2020-12-18 17:06:27 -0500

    if else statement for users password length

--- Develop/script.js
+++ Develop/script.js
@@ -18,26 +18,39 @@ uppers = letters.map(upLetters);
 
 var generateBtn = document.querySelector("#generate");
 
-console.log(generateBtn)
+get.addEventListener("click,", function () { 
+  pass = generatePassword();
+  document.getElementById("password").placeholder = pass;
+});
 
 
 
-// Write password to the #password input
-function generatePassword(characterCount, numbers, specialCharacters, upperCase, lowerCase) {
-  return "password"
+
+// function for generating password
+function generatePassword() {
+  
+  //Asks user 
+  var characterCount = parseInt(prompt("How many characters do you want your password to be?"));
+  
+  // Creating an if statement if user doesnt input allowed character amount
+  if (characterCount === null){
+    alert("Please input a number");
+  } else if (characterCount < 8) {
+    characterCount = parseInt(prompt("Password too short! Please input a number greater than 8."));
+  } else if (characterCount > 128){
+    characterCount = parseInt(prompt("Password too long! Please shorten to less than 128 characters."));
+  } else {
+    //User confirms 
+    var numbers = confirm("Do you want numbers in your password?");
+    var specialCharacters = confirm("Do you want special characters in your password?");
+    var upperCase = confirm("Do you want upper case in your password?");
+    var lowerCase = confirm("Do you want lower case in your password?");
+  };
 }
 
 function writePassword() {
   //parseInt makes the computer look for an integer
-  var characterCount = parseInt(prompt("How many characters do you want your password to be"));
   
-  var numbers = confirm("Do you want numbers in your password?");
-  
-  var specialCharacters = confirm("Do you want special characters in your password?");
-  
-  var upperCase = confirm("Do you want upper case in your password?");
-  
-  var lowerCase = confirm("Do you want lower case in your password?");
   
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
